c88ec715a6989f30554d150e5884fa5ca578cd9b
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-12-29 18:50:09 +0100

    Filtri applicati e cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -38,6 +40,29 @@ export default function Index() {
                 See all
               </Text>
             </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex flex-row gap-5">
+          <FeaturedCard />
+          <FeaturedCard />
+        </View>
+        <View className="mt-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Raccomandations
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Filters />
+
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
           </View>
         </View>
       </View>
